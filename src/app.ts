03c81b3819f9243547a4b0be60d8863e5f06a0f2
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-07 23:19:07 -0700

    working on app.ts. finished configuration

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,65 @@
 const userName = 'Bob';
 console.log(userName);
-const button = document.querySelector('button');
 ////const button = document.querySelector('button')!;
 
-function add(n1: number, n2: number): number | void {
-  if (n1 + n2 > 0) {
-    return n1 + n2
+function subtract(n1: number, n2: number): number | void {
+  if (n1 - n2 > 0) {
+    return n1 - n2
   }
   return;
 }
 
-function clickHandler(message: string) {
-  console.log('Clicked! ' + message)
+const person = {
+  firstName: 'Max',
+  age: 30
+};
+
+const notCopiedPerson = person //copies the pointer to the person object (middleman)
+const copiedPerson = { ...person }; // creates new object identical to person
+
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curRes, curVal) => {
+    return curRes + curVal
+  }, 0)
 }
 
-if (button) {
-  button.addEventListener('click', clickHandler.bind(null, 'You\'re welcome'))
-}
+const hobbies = ['Sports', 'Cooking', 'eating', 'beating'];
+
+const addedNumber = add(5, 4, 3, 2, 1);
+console.log(addedNumber);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+const { firstName: theName, age } = person;
+// the variable must be an existing name but can rename with :
+
+// const add = (a: number, b: number) => a + b //default arg
+// const sub = (a: number, b: number = 1) => a - b //default arg
+
+
+// const activeHobbies = ['Hiking', ...hobbies];
+
+// activeHobbies.push(...hobbies)
+
+// const clickHandler = (message: string) => console.log('Clicked! ' + message);
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// printOutput(sub(5))
+
+
+// const button = document.querySelector('button');
+// if (button) {
+//   button.addEventListener('click', event => console.log(event))
+// }
+
+
+// if (button) {
+//   button.addEventListener('click', clickHandler.bind(null, 'You\'re welcome'))
+// }
+
+
 
 
 // let userInput: unknown;
